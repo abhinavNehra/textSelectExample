6ef77bf527373e000263057463741a2596646e9d
Abhinav Nehra <abhinav.nehra@engagetalent.com>
2020-08-28 21:02:56 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,286 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import TextSelection from 'text-selection-react'
+import {search} from 'search-tree-child'
+import {createPdf} from 'create-pdf'
 
+console.log(search)
+
+console.log('createpdf --->>>---', createPdf)
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedText : []
+    }
+  }
+
+  toggleCollectionModal = (text) =>  {
+    console.log('text ----', text)
+    const selectedText = this.state.selectedText
+    selectedText.push(text)
+    this.setState({
+      selectedText
+    })
+  }
   render() {
+    console.log('-----', JSON.stringify(search(tree, 'node', ["name","text"], ["tree", "node"], true)))
     return (
-      <div className="App">
+      <div className="App" id="pdf">
+        <header className="App-header">
+        <input type="button" onClick={() => createPdf("pdf")}  value="create pdf"/>
+        <div style={{width: '100%', background: 'white', color: 'black'}}>
+        {/* { this.state.selectedText.length && (
+          this.state.selectedText.map(text => {
+            return <p> {text} </p>
+          })
+        )} */}
+        </div>
+          <p>Abinav Lea kasdjkfalsdjfk kasjdf aklsjdf ja s;dfkjasdl fjas dfaksjdfjaskdjfkljsdklfjklsjalfjkasjdfkljaksdlfdfasjdlkfjlasdf
+            adsfaksdjfjasjdfjasdfa
+            sdfjkasdjf jalsdjkfjlkasdjfkjsakldjfkljasdfa
+            sdfjaskljdfkjaskdjfkjaskldjfklajlksdjf;awleifjawioejfo;iasjfiojsaiojfiasjewifjoiawejfa
+
+          </p>
+          <p>Leakjsakdjfkajsd kajdklf aklsdklf jasjdfkj akldjkalsjdfja sj dfkljasdfa
+            dskfj ak sjdklfjajsdfjkladsjfasdf
+            asdjfkasldjfkjaskdjflja;gkljdfklj;aksjdfkljasdlkfjsjdfkljsdjflasjdfj
+            asdjfjasdljfal;jsdklfjalsdjfklasjldfjlasdfas
+            dfaksjdfjaskdjfkljsdklfjklsjalfjkasjdfkljaksdlf
+          </p>
+          <p>Leasadj ksadjflasdfjlkajeoifweiaio jfasjlksldf;asdkfjalsdkf 
+            asdfjaksdfajs;dflajsdlfjadsf
+            asdfjasdklflajsdklfa
+            sdfjalsdfjlasdkjfkljadsklfjkasdjkfjldasfa 
+            adkfjalksjdfkjskdjajf
+          </p>
+          <p>Leaj ksjdafjs  aklsjdfla;sdfklaj;l  aldsjfaj sdfkljalsjdklfj ajdsf jwioeuiruewiuriu dfkajsdf 
+            aksdkfjklasdjlf adkfklasfkjakdghjkahkaslkdsjaiejowfj 
+          </p>
+          <p>kllkfsd' 'asd aljsdkjadjfk   jsdfkjaskldjfkljsdkl  adksjf asdkfj adjf klajdkfj 
+          askdfjasjdklj a akdjfkjasd kjfksjd kjfkl sdklfja akdjf kaldfj lkajf'
+          </p>
+          <p>Learn Reacksajdkjaslkj kajsdkljKALJ LKDJALs ADSK LAsdkL;ASKDL;KAL;kf;l
+              ADL;KAL;DKL;ASKD;LAskdl;kaSD Asd LAKDL';FLJASKLJF  tLea
+         
+          </p>
+        </header>
+
+          <header className="App-header">
+          <p>Lea kasdjkfalsdjfk kasjdf aklsjdf ja s;dfkjasdl fjas dfaksjdfjaskdjfkljsdklfjklsjalfjkasjdfkljaksdlfdfasjdlkfjlasdf
+            adsfaksdjfjasjdfjasdfa
+            sdfjkasdjfjalsdjkfjlkasdjfkjsakldjfkljasdfa
+            sdfjaskljdfkjaskdjfkjaskldjfklajlksdjf;awleifjawioejfo;iasjfiojsaiojfiasjewifjoiawejfa
+
+          </p>
+          <p>Leakjsakdjfkajsd kajdklf aklsdklf jasjdfkj akldjkalsjdfja sj dfkljasdfa
+            dskfj ak sjdklfjajsdfjkladsjfasdf
+            asdjfkasldjfkjaskdjflja;gkljdfklj;aksjdfkljasdlkfjsjdfkljsdjflasjdfj
+            asdjfjasdljfal;jsdklfjalsdjfklasjldfjlasdfas
+            dfaksjdfjaskdjfkljsdklfjklsjalfjkasjdfkljaksdlf
+          </p>
+          <p>Leasadj ksadjflasdfjlkajeoifweiaio jfasjlksldf;asdkfjalsdkf 
+            asdfjaksdfajs;dflajsdlfjadsf
+            asdfjasdklflajsdklfa
+            sdfjalsdfjlasdkjfkljadsklfjkasdjkfjldasfa 
+            adkfjalksjdfkjskdjajf
+          </p>
+          <p>Leaj ksjdafjs  aklsjdfla;sdfklaj;l  aldsjfaj sdfkljalsjdklfj ajdsf jwioeuiruewiuriu dfkajsdf 
+            aksdkfjklasdjlf adkfklasfkjakdghjkahkaslkdsjaiejowfj 
+          </p>
+          <p>kllkfsd' 'asd aljsdkjadjfk   jsdfkjaskldjfkljsdkl  adksjf asdkfj adjf klajdkfj 
+          askdfjasjdklj a akdjfkjasd kjfksjd kjfkl sdklfja akdjf kaldfj lkajf'
+          </p>
+          <p>Learn Reacksajdkjaslkj kajsdkljKALJ LKDJALs ADSK LAsdkL;ASKDL;KAL;kf;l
+              ADL;KAL;DKL;ASKD;LAskdl;kaSD Asd LAKDL';FLJASKLJF  tLea
+         
+          </p>
+        </header>
+        <header className="App-header">
+          <p>Lea kasdjkfalsdjfk kasjdf aklsjdf ja s;dfkjasdl fjas dfaksjdfjaskdjfkljsdklfjklsjalfjkasjdfkljaksdlfdfasjdlkfjlasdf
+            adsfaksdjfjasjdfjasdfa
+            sdfjkasdjfjalsdjkfjlkasdjfkjsakldjfkljasdfa
+            sdfjaskljdfkjaskdjfkjaskldjfklajlksdjf;awleifjawioejfo;iasjfiojsaiojfiasjewifjoiawejfa
+
+          </p>
+          <p>Leakjsakdjfkajsd kajdklf aklsdklf jasjdfkj akldjkalsjdfja sj dfkljasdfa
+            dskfj ak sjdklfjajsdfjkladsjfasdf
+            asdjfkasldjfkjaskdjflja;gkljdfklj;aksjdfkljasdlkfjsjdfkljsdjflasjdfj
+            asdjfjasdljfal;jsdklfjalsdjfklasjldfjlasdfas
+            dfaksjdfjaskdjfkljsdklfjklsjalfjkasjdfkljaksdlf
+          </p>
+          <p>Leasadj ksadjflasdfjlkajeoifweiaio jfasjlksldf;asdkfjalsdkf 
+            asdfjaksdfajs;dflajsdlfjadsf
+            asdfjasdklflajsdklfa
+            sdfjalsdfjlasdkjfkljadsklfjkasdjkfjldasfa 
+            adkfjalksjdfkjskdjajf
+          </p>
+          <p>Leaj ksjdafjs  aklsjdfla;sdfklaj;l  aldsjfaj sdfkljalsjdklfj ajdsf jwioeuiruewiuriu dfkajsdf 
+            aksdkfjklasdjlf adkfklasfkjakdghjkahkaslkdsjaiejowfj 
+          </p>
+          <p>kllkfsd' 'asd aljsdkjadjfk   jsdfkjaskldjfkljsdkl  adksjf asdkfj adjf klajdkfj 
+          askdfjasjdklj a akdjfkjasd kjfksjd kjfkl sdklfja akdjf kaldfj lkajf'
+          </p>
+          <p>Learn Reacksajdkjaslkj kajsdkljKALJ LKDJALs ADSK LAsdkL;ASKDL;KAL;kf;l
+              ADL;KAL;DKL;ASKD;LAskdl;kaSD Asd LAKDL';FLJASKLJF  tLea
+         
+          </p>
+        </header>
+        <header className="App-header">
+          <p>Lea kasdjkfalsdjfk kasjdf aklsjdf ja s;dfkjasdl fjas dfaksjdfjaskdjfkljsdklfjklsjalfjkasjdfkljaksdlfdfasjdlkfjlasdf
+            adsfaksdjfjasjdfjasdfa
+            sdfjkasdjfjalsdjkfjlkasdjfkjsakldjfkljasdfa
+            sdfjaskljdfkjaskdjfkjaskldjfklajlksdjf;awleifjawioejfo;iasjfiojsaiojfiasjewifjoiawejfa
+
+          </p>
+          <p>Leakjsakdjfkajsd kajdklf aklsdklf jasjdfkj akldjkalsjdfja sj dfkljasdfa
+            dskfj ak sjdklfjajsdfjkladsjfasdf
+            asdjfkasldjfkjaskdjflja;gkljdfklj;aksjdfkljasdlkfjsjdfkljsdjflasjdfj
+            asdjfjasdljfal;jsdklfjalsdjfklasjldfjlasdfas
+            dfaksjdfjaskdjfkljsdklfjklsjalfjkasjdfkljaksdlf
+          </p>
+          <p>Leasadj ksadjflasdfjlkajeoifweiaio jfasjlksldf;asdkfjalsdkf 
+            asdfjaksdfajs;dflajsdlfjadsf
+            asdfjasdklflajsdklfa
+            sdfjalsdfjlasdkjfkljadsklfjkasdjkfjldasfa 
+            adkfjalksjdfkjskdjajf
+          </p>
+          <p>Leaj ksjdafjs  aklsjdfla;sdfklaj;l  aldsjfaj sdfkljalsjdklfj ajdsf jwioeuiruewiuriu dfkajsdf 
+            aksdkfjklasdjlf adkfklasfkjakdghjkahkaslkdsjaiejowfj 
+          </p>
+          <p>kllkfsd' 'asd aljsdkjadjfk   jsdfkjaskldjfkljsdkl  adksjf asdkfj adjf klajdkfj 
+          askdfjasjdklj a akdjfkjasd kjfksjd kjfkl sdklfja akdjf kaldfj lkajf'
+          </p>
+          <p>Learn Reacksajdkjaslkj kajsdkljKALJ LKDJALs ADSK LAsdkL;ASKDL;KAL;kf;l
+              ADL;KAL;DKL;ASKD;LAskdl;kaSD Asd LAKDL';FLJASKLJF  tLea
+         
+          </p>
+
+        </header><header className="App-header">
+          <p>Lea kasdjkfalsdjfk kasjdf aklsjdf ja s;dfkjasdl fjas dfaksjdfjaskdjfkljsdklfjklsjalfjkasjdfkljaksdlfdfasjdlkfjlasdf
+            adsfaksdjfjasjdfjasdfa
+            sdfjkasdjfjalsdjkfjlkasdjfkjsakldjfkljasdfa
+            sdfjaskljdfkjaskdjfkjaskldjfklajlksdjf;awleifjawioejfo;iasjfiojsaiojfiasjewifjoiawejfa
+
+          </p>
+          <p>Leakjsakdjfkajsd kajdklf aklsdklf jasjdfkj akldjkalsjdfja sj dfkljasdfa
+            dskfj ak sjdklfjajsdfjkladsjfasdf
+            asdjfkasldjfkjaskdjflja;gkljdfklj;aksjdfkljasdlkfjsjdfkljsdjflasjdfj
+            asdjfjasdljfal;jsdklfjalsdjfklasjldfjlasdfas
+            dfaksjdfjaskdjfkljsdklfjklsjalfjkasjdfkljaksdlf
+          </p>
+          <p>Leasadj ksadjflasdfjlkajeoifweiaio jfasjlksldf;asdkfjalsdkf 
+            asdfjaksdfajs;dflajsdlfjadsf
+            asdfjasdklflajsdklfa
+            sdfjalsdfjlasdkjfkljadsklfjkasdjkfjldasfa 
+            adkfjalksjdfkjskdjajf
+          </p>
+          <p>Leaj ksjdafjs  aklsjdfla;sdfklaj;l  aldsjfaj sdfkljalsjdklfj ajdsf jwioeuiruewiuriu dfkajsdf 
+            aksdkfjklasdjlf adkfklasfkjakdghjkahkaslkdsjaiejowfj 
+          </p>
+          <p>kllkfsd' 'asd aljsdkjadjfk   jsdfkjaskldjfkljsdkl  adksjf asdkfj adjf klajdkfj 
+          askdfjasjdklj a akdjfkjasd kjfksjd kjfkl sdklfja akdjf kaldfj lkajf'
+          </p>
+          <p>Learn Reacksajdkjaslkj kajsdkljKALJ LKDJALs ADSK LAsdkL;ASKDL;KAL;kf;l
+              ADL;KAL;DKL;ASKD;LAskdl;kaSD Asd LAKDL';FLJASKLJF  tLea
+         
+          </p>
+
+        </header>
+        <header className="App-header">
+          <p>Lea kasdjkfalsdjfk kasjdf aklsjdf ja s;dfkjasdl fjas dfaksjdfjaskdjfkljsdklfjklsjalfjkasjdfkljaksdlfdfasjdlkfjlasdf
+            adsfaksdjfjasjdfjasdfa
+            sdfjkasdjfjalsdjkfjlkasdjfkjsakldjfkljasdfa
+            sdfjaskljdfkjaskdjfkjaskldjfklajlksdjf;awleifjawioejfo;iasjfiojsaiojfiasjewifjoiawejfa
+
+          </p>
+          <p>Leakjsakdjfkajsd kajdklf aklsdklf jasjdfkj akldjkalsjdfja sj dfkljasdfa
+            dskfj ak sjdklfjajsdfjkladsjfasdf
+            asdjfkasldjfkjaskdjflja;gkljdfklj;aksjdfkljasdlkfjsjdfkljsdjflasjdfj
+            asdjfjasdljfal;jsdklfjalsdjfklasjldfjlasdfas
+            dfaksjdfjaskdjfkljsdklfjklsjalfjkasjdfkljaksdlf
+          </p>
+          <p>Leasadj ksadjflasdfjlkajeoifweiaio jfasjlksldf;asdkfjalsdkf 
+            asdfjaksdfajs;dflajsdlfjadsf
+            asdfjasdklflajsdklfa
+            sdfjalsdfjlasdkjfkljadsklfjkasdjkfjldasfa 
+            adkfjalksjdfkjskdjajf
+          </p>
+          <p>Leaj ksjdafjs  aklsjdfla;sdfklaj;l  aldsjfaj sdfkljalsjdklfj ajdsf jwioeuiruewiuriu dfkajsdf 
+            aksdkfjklasdjlf adkfklasfkjakdghjkahkaslkdsjaiejowfj 
+          </p>
+          <p>kllkfsd' 'asd aljsdkjadjfk   jsdfkjaskldjfkljsdkl  adksjf asdkfj adjf klajdkfj 
+          askdfjasjdklj a akdjfkjasd kjfksjd kjfkl sdklfja akdjf kaldfj lkajf'
+          </p>
+          <p>Learn Reacksajdkjaslkj kajsdkljKALJ LKDJALs ADSK LAsdkL;ASKDL;KAL;kf;l
+              ADL;KAL;DKL;ASKD;LAskdl;kaSD Asd LAKDL';FLJASKLJF  tLea
+         
+          </p>
+
+        </header>
+        <header className="App-header">
+          <p>Lea kasdjkfalsdjfk kasjdf aklsjdf ja s;dfkjasdl fjas dfaksjdfjaskdjfkljsdklfjklsjalfjkasjdfkljaksdlfdfasjdlkfjlasdf
+            adsfaksdjfjasjdfjasdfa
+            sdfjkasdjfjalsdjkfjlkasdjfkjsakldjfkljasdfa
+            sdfjaskljdfkjaskdjfkjaskldjfklajlksdjf;awleifjawioejfo;iasjfiojsaiojfiasjewifjoiawejfa
+
+          </p>
+          <p>Leakjsakdjfkajsd kajdklf aklsdklf jasjdfkj akldjkalsjdfja sj dfkljasdfa
+            dskfj ak sjdklfjajsdfjkladsjfasdf
+            asdjfkasldjfkjaskdjflja;gkljdfklj;aksjdfkljasdlkfjsjdfkljsdjflasjdfj
+            asdjfjasdljfal;jsdklfjalsdjfklasjldfjlasdfas
+            dfaksjdfjaskdjfkljsdklfjklsjalfjkasjdfkljaksdlf
+          </p>
+          <p>Leasadj ksadjflasdfjlkajeoifweiaio jfasjlksldf;asdkfjalsdkf 
+            asdfjaksdfajs;dflajsdlfjadsf
+            asdfjasdklflajsdklfa
+            sdfjalsdfjlasdkjfkljadsklfjkasdjkfjldasfa 
+            adkfjalksjdfkjskdjajf
+          </p>
+          <p>Leaj ksjdafjs  aklsjdfla;sdfklaj;l  aldsjfaj sdfkljalsjdklfj ajdsf jwioeuiruewiuriu dfkajsdf 
+            aksdkfjklasdjlf adkfklasfkjakdghjkahkaslkdsjaiejowfj 
+          </p>
+          <p>kllkfsd' 'asd aljsdkjadjfk   jsdfkjaskldjfkljsdkl  adksjf asdkfj adjf klajdkfj 
+          askdfjasjdklj a akdjfkjasd kjfksjd kjfkl sdklfja akdjf kaldfj lkajf'
+          </p>
+          <p>Learn Reacksajdkjaslkj kajsdkljKALJ LKDJALs ADSK LAsdkL;ASKDL;KAL;kf;l
+              ADL;KAL;DKL;ASKD;LAskdl;kaSD Asd LAKDL';FLJASKLJF  tLea
+         
+          </p>
+
+        </header>
+        <header className="App-header">
+          <p>Lea kasdjkfalsdjfk kasjdf aklsjdf ja s;dfkjasdl fjas dfaksjdfjaskdjfkljsdklfjklsjalfjkasjdfkljaksdlfdfasjdlkfjlasdf
+            adsfaksdjfjasjdfjasdfa
+            sdfjkasdjfjalsdjkfjlkasdjfkjsakldjfkljasdfa
+            sdfjaskljdfkjaskdjfkjaskldjfklajlksdjf;awleifjawioejfo;iasjfiojsaiojfiasjewifjoiawejfa
+
+          </p>
+          <p>Leakjsakdjfkajsd kajdklf aklsdklf jasjdfkj akldjkalsjdfja sj dfkljasdfa
+            dskfj ak sjdklfjajsdfjkladsjfasdf
+            asdjfkasldjfkjaskdjflja;gkljdfklj;aksjdfkljasdlkfjsjdfkljsdjflasjdfj
+            asdjfjasdljfal;jsdklfjalsdjfklasjldfjlasdfas
+            dfaksjdfjaskdjfkljsdklfjklsjalfjkasjdfkljaksdlf
+          </p>
+          <p>Leasadj ksadjflasdfjlkajeoifweiaio jfasjlksldf;asdkfjalsdkf 
+            asdfjaksdfajs;dflajsdlfjadsf
+            asdfjasdklflajsdklfa
+            sdfjalsdfjlasdkjfkljadsklfjkasdjkfjldasfa 
+            adkfjalksjdfkjskdjajf
+          </p>
+          <p>Leaj ksjdafjs  aklsjdfla;sdfklaj;l  aldsjfaj sdfkljalsjdklfj ajdsf jwioeuiruewiuriu dfkajsdf 
+            aksdkfjklasdjlf adkfklasfkjakdghjkahkaslkdsjaiejowfj 
+          </p>
+          <p>kllkfsd' 'asd aljsdkjadjfk   jsdfkjaskldjfkljsdkl  adksjf asdkfj adjf klajdkfj 
+          askdfjasjdklj a akdjfkjasd kjfksjd kjfkl sdklfja akdjf kaldfj lkajf'
+          </p>
+          <p>Learn Reacksajdkjaslkj kajsdkljKALJ LKDJALs ADSK LAsdkL;ASKDL;KAL;kf;l
+              ADL;KAL;DKL;ASKD;LAskdl;kaSD Asd LAKDL';FLJASKLJF  tLea
+         
+          </p>
+
+        </header>
         <header className="App-header">
           <p>Lea kasdjkfalsdjfk kasjdf aklsjdf ja s;dfkjasdl fjas dfaksjdfjaskdjfkljsdklfjklsjalfjkasjdfkljaksdlfdfasjdlkfjlasdf
             adsfaksdjfjasjdfjasdfa
@@ -37,20 +311,26 @@ class App extends Component {
          
           </p>
           <TextSelection
+            unmark={true}
+            unmarkText="Remove"
             events={[
-            {
+              {
                 text: 'Submit',
-                handler: (text, html) => {
+                handler: (html, text) => this.toggleCollectionModal(text) 
+                
+              },
+              {
+                text: 'facebook',
+                handler: (html, text) => {
                   console.log('text ----', text)
-                  console.log('html -----', html)
+                  console.log('html ----', html)
                 }
-               
-            }
+              }
             ]}
-            
+
             color={'purple'}
             colorText={true}
-        />
+          />
         </header>
       </div>
     );
@@ -58,3 +338,49 @@ class App extends Component {
 }
 
 export default App;
+
+
+const tree = [{
+  "name": "abhinav",
+  "text": "asdf",
+  "node" : [
+      {"name": "arjun"},
+      {"name": "ankur"}
+    ],
+  "tree": [
+      {"name": "arjun", "text": "nujra"},
+      {"name": "ankur", "text": "rukna"},
+      {
+          "name": "ankit",
+          "text": "tikna",
+          "tree": [
+              {"name": "akshay"},
+              {"name": "jainnode"}
+          ]
+      },{"name": "abhay",
+       "tree": [
+          {"name": "akshit"},
+          {"name": "pratyush"},
+          {
+            "name": "uday",
+            "tree": [
+                {"name": "amit",
+                  "node": [{ "text": "nodesearch"}]
+                },
+                {"name": "akilesh",
+                  "text": "asdf",
+                  "tree" : [{ "name": "search"}]
+                }
+            ]
+          }
+        ]
+      }
+    ]
+},
+{
+  "name": "neeraj",
+  "tree": [
+      {"name": "bana"}
+  ]
+}
+]
---
+++ src/pdf.js
@@ -0,0 +1,124 @@
+import jsPDF from 'jspdf'
+import html2canvas from 'html2canvas'
+
+
+// Function to create the pdf
+// accept id's in array
+async function createPdf(id) {
+
+    let pdf = new jsPDF({
+        orientation: 'p',
+        unit: 'mm',
+        format: 'letter',
+        compression: true
+    })
+
+    // Converting all the element into canvas and storing them in an array
+    const canvas = await Promise(html2canvas(document.querySelector(id), { scale: 2 }))
+
+    console.log(canvas)
+
+    let position = 0,
+        pages = 0,
+        width = 215.9, // Default pdf page width
+        height
+    const pageHeight = 297 // Default pdf page height
+
+
+    // Internal scalefactor is used to scale the pdf and improve the quality of the pdf
+    pdf.internal.scaleFactor = 2.25
+
+    // aspect height of the canvas
+    height = (canvas.height * width) / canvas.width
+
+    // fill the background of the pdf with the background color of the pdf
+    pdf.setFillColor(240, 242, 245)
+
+    // adding the border of the pdf
+    pdf.rect(
+        0,
+        0,
+        pdf.internal.pageSize.width,
+        pdf.internal.pageSize.height,
+        'F'
+    )
+
+    // Adding the canvas into the pdf
+    // here position start from the 0 (top) of the pdf
+    // width is 215.9 which is same as canvas width in mm
+    // height can be of any value i.e. either it can be of 200mm or of 500mm
+    // but as default pdf height is of 297
+    // So canvas will get printed upto the height of 297 only after that it get ignored
+    pdf.addImage(
+        canvas.toDataURL('image/png'),
+        'PNG',
+        0,
+        position,
+        width,
+        height,
+        undefined,
+        'FAST'
+    )
+
+    // adding border in the pdf
+    pdf.rect(
+        0,
+        0,
+        pdf.internal.pageSize.width,
+        pdf.internal.pageSize.height,
+        'S'
+    )
+
+    // If the aspect height of the canvas is greater than 297mm then portion after 297mm in first page get
+    // ignored. To print the remaining portion we have to find in how many pages it get printed
+    // So we decrease the 297mm from the canvas height as it already get printed and find in how many pages
+    // it get printed
+    pages = Math.ceil((height - pageHeight) / pageHeight)
+
+    // printed the remaining portion of the canvas on the pdf
+    for (let i = 1; i <= pages; i++) {
+        // adding new page
+        pdf.addPage()
+
+        // set background color
+        pdf.setFillColor(240, 242, 245)
+        // adding border of pdf
+        pdf.rect(
+            0,
+            0,
+            pdf.internal.pageSize.width,
+            pdf.internal.pageSize.height,
+            'F'
+        )
+
+        // It is the important part of the this function
+        // So position can be positive or negative
+        // if position is in positive then it skip the height of the pdf from the top and then add the canvas
+        //like eg if position is 50mm then it skip the 50mm from top of the pdf page and then start adding the
+        // content
+        // if position is in negative then it skip the height of the canvas/element from the canvas top and  then
+        // it start adding the content of the canvas like eg if position is -300 then if skip 300mm  of the canvas
+        // height and then start adding the remaining content to the pdf
+        position = -1 * (pageHeight * i) + 17 * i
+
+        // adding the content to the pdf
+        pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, position, width, height)
+        // adding border
+        pdf.rect(
+            0,
+            0,
+            pdf.internal.pageSize.width,
+            pdf.internal.pageSize.height,
+            'S'
+        )
+    }
+
+    pdf.save(`check.pdf`, { returnPromise: true }).then(() => {
+           
+        console.log('done')
+     })
+}
+
+module.exports = function (id) {
+    createPdf(id)
+}
